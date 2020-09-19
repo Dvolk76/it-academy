234b15747b2a2f94ff9ab29be37f41c6f214417c
Dmitry Volkov <dvolk18@gmail.com>
2020-09-20 01:26:49 +0300

    Кое как закончил)

--- Домашка 4.1.py
+++ Домашка 4.1.py
@@ -7,6 +7,12 @@ print("2. ", int(a) or int(b) or int(c) or "Введены все нули!!!")
 if a > b + c:
     print("3. ", a - b - c)
 else:
-    print("3. ", b + c - a)
+    print("3.\n4. ", b + c - a)
 if a > 50 and (b > a or c > a):
-    print("4. ", "Вася")
+    print("5. ", "Вася")
+else:
+    print("5. ")
+if a > 5 or b == 7 and c == 7:
+    print("6. ", "Петя")
+else:
+    print("6. ")
