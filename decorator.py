50e69191be382ea023c2927d29d514cbe8bb06f3
Dmitry Volkov <dvolk18@gmail.com>
2020-09-22 22:32:25 +0300

    v 1.01

--- decorator.py
+++ decorator.py
@@ -1,18 +1,22 @@
 
 def auth_required(func):
-    def inn(n):
+    def inn(number):
         creds = {'aaa': '111', 'login': 'pass', 'admin': 'admin'}
         login = input('log\n- ')
         parol = input('pass\n- ')
         para = (login, parol)
         if para in creds.items():
             print('Пароль верный')
-        else: print('Неверный пароль')
-        func(n)
+        else:
+            print('Неверный пароль')
+        func(number)
     return inn
 
-def square(n):
-    print(n**2)
 
-square = auth_required(square)
+@auth_required
+def square(x):
+    print(x**2)
+
+
+# square = auth_required(square)
 square(int(input('Введите число которое желаете возвести в квадрат \n- ')))
