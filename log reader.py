2af1417fb47d3b001a51894978baceda0bf3e703
Dmitry Volkov <dvolk18@gmail.com>
2020-09-27 20:12:23 +0300

    v 1

--- log reader.py
+++ log reader.py
@@ -3,21 +3,27 @@
 # 3. Какие браузеры обращались. Для простоты берем запись почти в конце строки (Firefox/34.0) и подобное
 # 4. Сколько запросов от каждого такого браузера
 
-
+from collections import Counter
+from pprint import pprint
 fp = open("log5000.txt", 'r')
-i = 0
 ip = []
-brows = []
+browsers = []
+browsers_names = {"Chrome": 0, "Safari": 0, "Firefox": 0, "Edge": 0, "Opera": 0}
 while True:
-    i = i + 1
+
     for line in fp.readlines():
         print(line.split())
         xxx = line.split()
         ip.append(xxx[0])
-        brows.append(xxx[-2])
-        brows_s = set(brows)
-        print("всего запросов", len(ip))
+        browsers.append(xxx[-2])
+        browsers_s = set(browsers)
+        print("Всего запросов - ", len(ip))
         yyy = set(ip)
-        print("всего уникальных ип", len(yyy))
-        print(len(brows_s))
+        print("Всего уникальных ip - ", len(yyy))
+        print("Всего уникальных браузеров - ", len(browsers_s))
+        d = dict.fromkeys(browsers_s)
+        imena = d.keys()
+        print('Обращались вот эти браузеры - ', imena)
+        c = Counter(browsers)
+        pprint(c)
 
