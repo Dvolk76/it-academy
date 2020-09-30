fb0b78c0343fd77b9a322fc39edd585d9bfb429f
Dmitry Volkov <dvolk18@gmail.com>
2020-09-30 19:35:33 +0300

    v 0.01 (start)

---
+++ clock.py
@@ -0,0 +1,9 @@
+def get_current_time():
+    #get and return current time some how
+    pass
+    #return time
+
+if __name__ == '__main__':
+    while True:
+        # TODO set curretnt time
+        current_time = get_current_time()
