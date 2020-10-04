080d9d35e73a25b25bec180757d643ed3e01c5ca
Dmitry Volkov <dvolk18@gmail.com>
2020-10-04 20:37:34 +0300

    v 1.0 ne rabotaet clear

--- clock.py
+++ clock.py
@@ -1,21 +1,109 @@
-def get_current_time():
-    #get and return current time some how
-    pass
-    #return time
+import datetime
+import time
+import os
 
-def print_digits(current_time):
-    pass
+l_0 = {
+    0: '███████',
+    1: '     ██',
+    2: '███████',
+    3: '███████',
+    4: '█     █',
+    5: '███████',
+    6: '███████',
+    7: '███████',
+    8: '███████',
+    9: '███████'
+}
+
+l_1 = {
+    0: '█     █',
+    1: '      █',
+    2: '      █',
+    3: '      █',
+    4: '█     █',
+    5: '█      ',
+    6: '█      ',
+    7: '      █',
+    8: '█     █',
+    9: '█     █'
+}
+
+l_2 = {
+    0: '█     █',
+    1: '      █',
+    2: '███████',
+    3: ' ██████',
+    4: '███████',
+    5: '███████',
+    6: '███████',
+    7: '      █',
+    8: '███████',
+    9: '███████'
+}
+
+l_3 = {
+    0: '█     █',
+    1: '      █',
+    2: '█      ',
+    3: '      █',
+    4: '      █',
+    5: '      █',
+    6: '█     █',
+    7: '      █',
+    8: '█     █',
+    9: '      █',
+}
+l_4 = {
+    0: '███████',
+    1: '      █',
+    2: '███████',
+    3: '███████',
+    4: '      █',
+    5: '███████',
+    6: '███████',
+    7: '      █',
+    8: '███████',
+    9: '███████'
+}
+
+
+def get_current_time():
+    now = datetime.datetime.now()
+    now_str = str(now)
+    current_time = now_str[11:19]
+    return current_time
+
+
+def print_digits(time_now):
+    hour_1 = int(time_now[0])
+    hour_2 = int(time_now[1])
+    min_1 = int(time_now[3])
+    min_2 = int(time_now[4])
+    sec_1 = int(time_now[6])
+    sec_2 = int(time_now[7])
+    if int(time_now[7]) % 2 == 0:
+        a = ' █ '
+    else:
+        a = '   '
+
+    print(l_0[hour_1], l_0[hour_2], '   ', l_0[min_1], l_0[min_2], '   ', l_0[sec_1], l_0[sec_2])
+    print(l_1[hour_1], l_1[hour_2], a, l_1[min_1], l_1[min_2], a, l_1[sec_1], l_1[sec_2])
+    print(l_2[hour_1], l_2[hour_2], '   ', l_2[min_1], l_2[min_2], '   ', l_2[sec_1], l_2[sec_2])
+    print(l_3[hour_1], l_3[hour_2], a, l_3[min_1], l_3[min_2], a, l_3[sec_1], l_3[sec_2])
+    print(l_4[hour_1], l_4[hour_2], '   ', l_4[min_1], l_4[min_2], '   ', l_4[sec_1], l_4[sec_2],)
+    print('\n')
 
 def clear_screen():
-    pass
+    os.system("clear")
+
 
 def sleep_for_a_while(period):
-    pass
+    time.sleep(period)
+
 
 if __name__ == '__main__':
     while True:
-        # TODO set curretnt time
-        current_time = get_current_time()
-        print_digits(current_time)
+        time_now = str(get_current_time())
+        print_digits(time_now)
+        sleep_for_a_while(1)
         clear_screen()
-        sleep_for_a_while(0.3)
